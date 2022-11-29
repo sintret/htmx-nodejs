4ce79436291ea067281ebb612971f8817d8fecdf
sintret <sintret@gmail.com>
2022-11-29 10:12:27 +0700

    delete row

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  res.locals.css = '';
+  res.locals.script = '';
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/click', function(req, res, next) {
-  res.render('click', { title: 'Express' });
+router.get('/click', function (req, res, next) {
+    res.render('click', {title: 'Express'});
 });
 
-router.post('/click', (req,res)=>{
-  res.send("oke lah")
+router.post('/click', (req, res) => {
+    res.send("oke lah")
 })
 
-router.get('/edit', (req,res)=>{
-  res.send(`<form hx-put="/edit" hx-target="this" hx-swap="outerHTML">
+router.get('/edit', (req, res) => {
+    res.send(`<form hx-put="/edit" hx-target="this" hx-swap="outerHTML">
   <div>
     <label>First Name</label>
     <input type="text" name="firstName" value="Joe">
@@ -33,9 +33,9 @@ router.get('/edit', (req,res)=>{
 </form> `)
 })
 
-router.put('/edit', (req,res)=>{
-  var body = req.body;
-  res.send(`<div hx-target="this" hx-swap="outerHTML">
+router.put('/edit', (req, res) => {
+    var body = req.body;
+    res.send(`<div hx-target="this" hx-swap="outerHTML">
  <div><label>First Name</label>: ${body.firstName}</div>
     <div><label>Last Name</label>: ${body.lastName}</div>
     <div><label>Email</label>: ${body.email}</div>
@@ -44,20 +44,55 @@ router.put('/edit', (req,res)=>{
     </button></div>`)
 })
 
-
-
-router.get('/bulk-update', (req,res) => {
-  res.render('bulk-update')
+router.get('/bulk-update', (req, res) => {
+    res.render('bulk-update')
 })
 
-router.put('/activate', (req,res)=>{
-  var body = req.body;
-  res.send(body)
+router.put('/activate', (req, res) => {
+    var body = req.body;
+    res.send(body)
 })
 
-router.put('/deactivate', (req,res)=>{
-  var body = req.body;
-  res.send(body)
+router.put('/deactivate', (req, res) => {
+    var body = req.body;
+    res.send(body)
 })
+
+
+router.get('/click-to-load', (req, res) => {
+    var page = parseInt(req.query.page) || 0;
+    console.log(page)
+    if (page) {
+        var html = '';
+        for (var i = 0; i < 4; i++) {
+            var x = i + page;
+            html += `<tr><td>Agent Smith</td><td>void${x}@null.org</td><td>${x}</td></tr>`
+        }
+        html += `<tr id="replaceMe"><td colspan="3"><button class="btn btn-primary" hx-get="/click-to-load?page=${x+1}" hx-target="#replaceMe" hx-swap="outerHTML">Load More Agents...<img class="htmx-indicator" src="/images/bars.svg"></button></td></tr>`
+
+        res.send(html)
+
+    } else {
+        res.render('click-to-load')
+    }
+})
+
+router.get('/delete-row', (req, res) => {
+  res.render('delete-row',{
+      css : 'delete-row.css'
+  })
+})
+
+router.delete('/delete-row/:row', (req, res) => {
+  var row = req.params.row;
+  res.send(``)
+})
+
+
+router.get('/edit-row', (req, res) => {
+  res.render('edit-row')
+})
+
+
 
 module.exports = router;
